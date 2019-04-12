c3d92d0e761e2c1e3dc69a7c511ab3a2b3f7c6ac
sam_hach <samir.hachemi@umontreal.com>
2019-04-12 19:03:22 -0400

    tout

--- src/views/vendeur/Vendeur_View_Controller.java
+++ src/views/vendeur/Vendeur_View_Controller.java
@@ -200,7 +200,61 @@ public class Vendeur_View_Controller implements Serializable {
 					Utilitaires.buildTableCol("CATEGORIE"), Utilitaires.buildTableCol("ETAT"));
 			tv_queries.setItems(data);
 		}
-		
+		if(cb_queries.getSelectionModel().isSelected(1))
+		{
+			ObservableList<Produit> data = req_c.getAllAnnoncesFilter(0, null);
+
+			tv_queries.getColumns().addAll(Utilitaires.buildTableCol("NOM"),Utilitaires.buildTableCol("DESCRIPTION"),
+					Utilitaires.buildTableCol("PRIX"), Utilitaires.buildTableCol("MAEQUE"),
+					Utilitaires.buildTableCol("LONGUEUR"), Utilitaires.buildTableCol("LARGEUR"),
+					Utilitaires.buildTableCol("PROFONDEUR"), Utilitaires.buildTableCol("TAILLE"), 
+					Utilitaires.buildTableCol("CATEGORIE"), Utilitaires.buildTableCol("ETAT"));
+			tv_queries.setItems(data);
+		}
+		if(cb_queries.getSelectionModel().isSelected(2))
+		{
+			ObservableList<Produit> data = req_c.getAllProducts();
+
+			tv_queries.getColumns().addAll(Utilitaires.buildTableCol("NOM"),Utilitaires.buildTableCol("DESCRIPTION"),
+					Utilitaires.buildTableCol("PRIX"), Utilitaires.buildTableCol("MAEQUE"),
+					Utilitaires.buildTableCol("LONGUEUR"), Utilitaires.buildTableCol("LARGEUR"),
+					Utilitaires.buildTableCol("PROFONDEUR"), Utilitaires.buildTableCol("TAILLE"), 
+					Utilitaires.buildTableCol("CATEGORIE"), Utilitaires.buildTableCol("ETAT"));
+			tv_queries.setItems(data);
+		}
+		if(cb_queries.getSelectionModel().isSelected(3))
+		{
+			ObservableList<Produit> data = req_c.getAllProducts();
+
+			tv_queries.getColumns().addAll(Utilitaires.buildTableCol("NOM"),Utilitaires.buildTableCol("DESCRIPTION"),
+					Utilitaires.buildTableCol("PRIX"), Utilitaires.buildTableCol("MAEQUE"),
+					Utilitaires.buildTableCol("LONGUEUR"), Utilitaires.buildTableCol("LARGEUR"),
+					Utilitaires.buildTableCol("PROFONDEUR"), Utilitaires.buildTableCol("TAILLE"), 
+					Utilitaires.buildTableCol("CATEGORIE"), Utilitaires.buildTableCol("ETAT"));
+			tv_queries.setItems(data);
+		}
+		if(cb_queries.getSelectionModel().isSelected(4))
+		{
+			ObservableList<Produit> data = req_c.getAllProducts();
+
+			tv_queries.getColumns().addAll(Utilitaires.buildTableCol("NOM"),Utilitaires.buildTableCol("DESCRIPTION"),
+					Utilitaires.buildTableCol("PRIX"), Utilitaires.buildTableCol("MAEQUE"),
+					Utilitaires.buildTableCol("LONGUEUR"), Utilitaires.buildTableCol("LARGEUR"),
+					Utilitaires.buildTableCol("PROFONDEUR"), Utilitaires.buildTableCol("TAILLE"), 
+					Utilitaires.buildTableCol("CATEGORIE"), Utilitaires.buildTableCol("ETAT"));
+			tv_queries.setItems(data);
+		}
+		if(cb_queries.getSelectionModel().isSelected(5))
+		{
+			ObservableList<Produit> data = req_c.getAllProducts();
+
+			tv_queries.getColumns().addAll(Utilitaires.buildTableCol("NOM"),Utilitaires.buildTableCol("DESCRIPTION"),
+					Utilitaires.buildTableCol("PRIX"), Utilitaires.buildTableCol("MAEQUE"),
+					Utilitaires.buildTableCol("LONGUEUR"), Utilitaires.buildTableCol("LARGEUR"),
+					Utilitaires.buildTableCol("PROFONDEUR"), Utilitaires.buildTableCol("TAILLE"), 
+					Utilitaires.buildTableCol("CATEGORIE"), Utilitaires.buildTableCol("ETAT"));
+			tv_queries.setItems(data);
+		}
 	}
 	
 	
